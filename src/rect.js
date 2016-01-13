95e15fc5436ad6d62d99e511f6ea89d6b6fa810e
Aysel Afsar <aysel.afsar@gmail.com>
2016-01-13 15:57:01 -0500

    Add additional math operations
    - Get intersection of two 2d lines
    - Get intersection of two rectangles
    - Add sign operation in math module

--- src/rect.js
+++ src/rect.js
@@ -78,10 +78,110 @@ var cornerstoneMath = (function (cornerstoneMath) {
         return minDistance;
     }
 
+    // Returns rectangle points
+    function rectToPoints (rect) {
+        var rectPoints = {
+            top: rect.top,
+            left: rect.left,
+            right: rect.left + rect.width,
+            bottom: rect.top + rect.height
+        };
+
+        return rectPoints;
+    }
+
+    // Returns whether two rectangles are intersected
+    function doesIntersect (rect1, rect2) {
+        var intersectLeftRight;
+        var intersectTopBottom;
+
+        var rect1Points = rectToPoints(rect1);
+        var rect2Points = rectToPoints(rect2);
+
+        if (rect1.width >= 0) {
+            if (rect2.width >= 0)
+                intersectLeftRight = !((rect1Points.right <= rect2Points.left) || (rect2Points.right <= rect1Points.left));
+            else
+                intersectLeftRight = !((rect1Points.right <= rect2Points.right) || (rect2Points.left <= rect1Points.left));
+        } else {
+            if (rect2.width >= 0)
+                intersectLeftRight = !((rect1Points.left <= rect2Points.left) || (rect2Points.right <= rect1Points.right));
+            else
+                intersectLeftRight = !((rect1Points.left <= rect2Points.right) || (rect2Points.left <= rect1Points.right));
+        }
+
+        if (rect1.height >= 0) {
+            if (rect2.height >= 0)
+                intersectTopBottom = !((rect1Points.bottom <= rect2Points.top) || (rect2Points.bottom <= rect1Points.top));
+            else
+                intersectTopBottom = !((rect1Points.bottom <= rect2Points.bottom) || (rect2Points.top <= rect1Points.top));
+        } else {
+            if (rect2.height >= 0)
+                intersectTopBottom = !((rect1Points.top <= rect2Points.top) || (rect2Points.bottom <= rect1Points.bottom));
+            else
+                intersectTopBottom = !((rect1Points.top <= rect2Points.bottom) || (rect2Points.top <= rect1Points.bottom));
+        }
+
+        return intersectLeftRight && intersectTopBottom;
+    }
+
+    // Returns intersection points of two rectangles
+    function getIntersectionRect(rect1, rect2) {
+        var intersectPoints = {};
+
+        if (!doesIntersect(rect1, rect2)) {
+            return;
+        }
+
+        var rect1Points = rectToPoints(rect1);
+        var rect2Points = rectToPoints(rect2);
+
+        if (rect1.width >= 0) {
+            if (rect2.width >= 0) {
+                intersectPoints.left = Math.max(rect1Points.left, rect2Points.left);
+                intersectPoints.right = Math.min(rect1Points.right, rect2Points.right);
+            } else {
+                intersectPoints.left = Math.max(rect1Points.left, rect2Points.right);
+                intersectPoints.right = Math.min(rect1Points.right, rect2Points.left);
+            }
+        } else {
+            if (rect2.width >= 0) {
+                intersectPoints.left = Math.min(rect1Points.left, rect2Points.right);
+                intersectPoints.right = Math.max(rect1Points.right, rect2Points.left);
+            } else {
+                intersectPoints.left = Math.min(rect1Points.left, rect2Points.left);
+                intersectPoints.right = Math.max(rect1Points.right, rect2Points.right);
+            }
+        }
+
+        if (rect1.height >= 0) {
+            if (rect2.height >= 0) {
+                intersectPoints.top = Math.max(rect1Points.top, rect2Points.top);
+                intersectPoints.bottom = Math.min(rect1Points.bottom, rect2Points.bottom);
+            } else {
+                intersectPoints.top = Math.max(rect1Points.top, rect2Points.bottom);
+                intersectPoints.bottom = Math.min(rect1Points.bottom, rect2Points.top);
+            }
+        } else {
+            if (rect2.height >= 0) {
+                intersectPoints.top = Math.min(rect1Points.top, rect2Points.bottom);
+                intersectPoints.bottom = Math.max(rect1Points.bottom, rect2Points.top);
+            } else {
+                intersectPoints.top = Math.min(rect1Points.top, rect2Points.top);
+                intersectPoints.bottom = Math.max(rect1Points.bottom, rect2Points.bottom);
+            }
+        }
+
+        return intersectPoints;
+
+    }
+
     // module exports
     cornerstoneMath.rect =
     {
-        distanceToPoint : distanceToPoint
+        distanceToPoint : distanceToPoint,
+        getIntersectionRect : getIntersectionRect
+
     };
 
 
